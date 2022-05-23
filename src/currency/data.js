e80e1cf2f9e86c0780f710ba5bd6fe9e4d1bc968
Tushar Bodara <tushar.bodarya@purpleleafstrategy.com>
2022-05-23 12:13:16 +0200

    Update data

--- src/currency/data.js
+++ src/currency/data.js
@@ -1,27 +1,9 @@
 const loadCurrency = [
   {
-    currency: 'USD',
-    name:'doller',
-    symbol: '$',
-    value: 1
-  },
-  {
     currency: 'IND',
     name:'rupees',
     symbol: '₹',
-    value: 60.0
-  },
-  {
-    currency: 'EUR',
-    name:'euro',
-    symbol: '€',
-    value: 52.00
-  },
-  {
-    currency: 'GBP',
-    name:'pound',
-    symbol: '£',
-    value: 62.00
+    value: 1
   }
 ]
 

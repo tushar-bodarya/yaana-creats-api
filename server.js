8b4637d788ce4fc05e9e9cba229ad0ced0c913e7
Tushar Bodarya <tushar.bodarya85@gmail.com>
2022-06-09 12:55:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require('apollo-server')
 const typeDefs = require('./src/product/schema')
 const resolvers = require('./src/product/resolver')
+const cors = require('cors');
 
 const server = new ApolloServer({
   typeDefs,
@@ -8,7 +9,7 @@ const server = new ApolloServer({
   introspection: true,
   playground: true,
 })
-
+server.use(cors);
 server
   .listen({ port: process.env.PORT || 4000 })
   .then(({ url }) => console.log('Server is running on localhost:4000', url))
